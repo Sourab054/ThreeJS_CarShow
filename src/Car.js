61ad9b9dbfe22de96ae77861fde350b675b34a96
Sourab Patil <sourab.patil@wowlabz.comgit config --global user.name Sourab>
2023-01-20 09:50:53 +0530

    final commit

---
+++ src/Car.js
@@ -0,0 +1,34 @@
+import { useFrame, useLoader } from "@react-three/fiber";
+import React, { useEffect } from "react";
+import { Mesh } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const Car = () => {
+  const gltf = useLoader(GLTFLoader, "models/car/scene.gltf");
+
+  useEffect(() => {
+    gltf.scene.scale.set(0.005, 0.005, 0.005);
+    gltf.scene.position.set(0, -0.035, 0);
+    gltf.scene.traverse((obj) => {
+      if (obj instanceof Mesh) {
+        obj.castShadow = true;
+        obj.receiveShadow = true;
+        obj.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+
+  useFrame((state, delta) => {
+    let t = state.clock.getElapsedTime();
+
+    let group = gltf.scene.children[0].children[0].children[0];
+    group.children[0].rotation.x = t * 2;
+    group.children[2].rotation.x = t * 2;
+    group.children[4].rotation.x = t * 2;
+    group.children[6].rotation.x = t * 2;
+  });
+
+  return <primitive object={gltf.scene} />;
+};
+
+export default Car;
